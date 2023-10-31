7f87f0cf4e0e5d43b4ea939e5de3acf61678c31f
Michael Janczyk <190198+planetmija@users.noreply.github.com>
2023-10-31 13:22:06 +0100

    usr: local: bin: bwidm_rest_ssh: remove other HTTP code checks.     Only check if not "200 OK".     More linting.

--- usr/local/bin/bwidm_rest_ssh.py
+++ usr/local/bin/bwidm_rest_ssh.py
@@ -1,68 +1,80 @@
 #!/usr/bin/env python3
+"""
+Fetches all active SSH keys of a bwIDM user for a bwIDM service.
+The SSH command can be used to use FIDO2 SSH keys without OTP.
+"""
 
-import sys
+import argparse
+import base64
+import configparser
 import os
 import re
-import base64
+import sys
+
 import requests
-import configparser
-import argparse
 
 # Config file location
-config_file = "/usr/local/etc/bwidm-rest-ssh.conf"
-min_user_id = 900000
+CONFIG_FILE = "/usr/local/etc/bwidm_rest_ssh.conf"
+MIN_USER_ID = 900000
+FIDO2_KEY_NAME = "FIDO2"
 
 
 def exit_with_msg(exit_code, *messages):
+    """Function prints message and exits."""
     for msg in messages:
         print(msg, file=sys.stderr)
     sys.exit(exit_code)
 
 
-def check_user_name(ssh_user):
-    if not re.fullmatch(r"\w{1,20}", ssh_user):
-        exit_with_msg(8, f"Not a valid user name: {ssh_user}")
-    return ssh_user
+def check_user_name(ssh_usr):
+    """Function checks if username is valid."""
+    if not re.fullmatch(r"\w{1,12}", ssh_usr):
+        exit_with_msg(41, f"Not a valid user name: {ssh_usr}")
+    return ssh_usr
 
 
-def check_user_id(user_id):
-    if re.fullmatch(r"\d{6}", user_id):
-        user_id = int(user_id)
-        if user_id > min_user_id:
-            return user_id
-    exit_with_msg(9, f"Not a bwIDM User ID: {user_id}")
+def check_user_id(uid):
+    """Check if user ID is within range."""
+    if re.fullmatch(r"\d{6}", uid):
+        uid = int(uid)
+        if uid > MIN_USER_ID:
+            return uid
+    exit_with_msg(31, f"Not a bwIDM User ID: {uid}")
 
 
-# Check, if submitted key has string "sk-ssh-ed25519@openssh.com"
-# Returns base64 encoded part and comment at end
-def get_fido2_public_key(fido2_public_key):
-    # We use bwIDM command key functionality to submit FIDO2 SSH keys
-    # Use "FIDO2" for command, IP range is ignored
-    # Check for string "sk-ssh-ed25519@openssh.com"
-    key = re.findall(
-        r'command="FIDO2",from=".*"\s+sk-ssh-ed25519@openssh.com\s+([A-Za-z0-9+/=]+)',
-        fido2_public_key,
+def get_fido2_public_key(fido2_pk):
+    """
+    Check, if submitted key has string 'sk-ssh-ed25519@openssh.com'
+    Returns base64 encoded part and comment at end
+    We use bwIDM command key functionality to submit FIDO2 SSH keys
+    Use 'FIDO2_KEY_NAME' for command, IP range is ignored
+    Check for string 'sk-ssh-ed25519@openssh.com'
+    """
+    ssh_key = re.findall(
+        r'command="{FIDO2_KEY_NAME}",from=".*"\s+sk-ssh-ed25519@openssh.com\s+([A-Za-z0-9+/=]+)',
+        fido2_pk,
     )
     key_comment = re.findall(
         r"sk-ssh-ed25519@openssh.com\s+[A-Za-z0-9+/=]+\s+([A-Za-z0-9+/=.@-]+)",
-        fido2_public_key,
+        fido2_pk,
     )
     # If string was found, use next space separated part for SSH public key
-    if key:
-        return key[0], key_comment[0]
+    if ssh_key:
+        return ssh_key[0], key_comment[0]
     else:
         return None
 
 
-# Decode submitted public key and check, if key has string "sk-ssh-ed25519@openssh.com"
-def decode_fido2_public_key(fido2_public_key):
+def decode_fido2_public_key(fido2_pub_key):
+    """Decode submitted public key and check
+    if key has string 'sk-ssh-ed25519@openssh.com'"""
     # First get base64 encoded key part [0] and comment [1]
-    fido2_key = get_fido2_public_key(fido2_public_key)
+    fido2_key = get_fido2_public_key(fido2_pub_key)
     if fido2_key:
-        fido2_public_key = fido2_key[0]
+        fido2_pub_key = fido2_key[0]
         fido2_key_comment = fido2_key[1]
         # Decode public key
-        decoded_key = base64.b64decode(fido2_public_key)
+        decoded_key = base64.b64decode(fido2_pub_key)
         # Remove the first 4 bytes
         decoded_key = decoded_key[4:]
         # Find the index of the first null byte
@@ -76,7 +88,7 @@ def decode_fido2_public_key(fido2_public_key):
         match = re.search(r"sk-ssh-ed25519@openssh.com", utf8_key)
         # If string matches, return FIDO2 key
         if match:
-            return match[0], fido2_public_key, fido2_key_comment
+            return match[0], fido2_pub_key, fido2_key_comment
         else:
             return None
 
@@ -86,14 +98,15 @@ parser = argparse.ArgumentParser(description="Process some stuff.")
 parser.add_argument("ssh_user", type=check_user_name, help="SSH User Name")
 parser.add_argument("user_id", type=check_user_id, help="SSH User ID")
 args = parser.parse_args()
-
+ssh_user: str
+user_id: int
 ssh_user = args.ssh_user
 user_id = args.user_id
 
 # Local user, skip AttributeQuery (Access granted)
 authorized_keys_path = f"/etc/ssh/authorized_keys.d/{ssh_user}"
 if os.path.exists(authorized_keys_path):
-    with open(authorized_keys_path, "r") as file:
+    with open(authorized_keys_path, "r", encoding="utf-8") as file:
         print(file.read())
     sys.exit(0)
 
@@ -112,23 +125,29 @@ if os.path.exists(authorized_keys_path):
 # Read config file
 config = configparser.ConfigParser()
 try:
-    config.read_file(open(config_file))
-    max_time = config.getint("DEFAULT", "max_time", fallback=10)
-    reg_host = config["REST"]["reg_host"]
-    rest_user = config["REST"]["rest_user"]
-    rest_pw = config["REST"]["rest_pw"]
-    ssn = config["SSN"]["ssn"]
-except:
-    exit_with_msg(12, f"Can not read config file {config_file}")
+    with open(CONFIG_FILE, "r", encoding="utf-8") as conf:
+        max_time: int
+        reg_host: str
+        rest_user: str
+        rest_pw: str
+        ssn: str
+        config.read_file(conf)
+        max_time = config.getint("DEFAULT", "max_time", fallback=10)
+        reg_host = config["REST"]["reg_host"]
+        rest_user = config["REST"]["rest_user"]
+        rest_pw = config["REST"]["rest_pw"]
+        ssn = config["SSN"]["ssn"]
+except OSError:
+    exit_with_msg(21, f"Can not read config file {CONFIG_FILE}")
 
 if not reg_host:
-    exit_with_msg(14, "Config variable reg_host is empty")
+    exit_with_msg(22, "Config variable reg_host is empty")
 elif not rest_user:
-    exit_with_msg(15, "Config variable rest_user is empty")
+    exit_with_msg(23, "Config variable rest_user is empty")
 elif not rest_pw:
-    exit_with_msg(16, "Config variable rest_pw is empty")
+    exit_with_msg(24, "Config variable rest_pw is empty")
 elif not ssn:
-    exit_with_msg(17, "Config variable SID is empty")
+    exit_with_msg(25, "Config variable SID is empty")
 
 try:
     response = requests.get(
@@ -138,7 +157,7 @@ try:
     )
     response.raise_for_status()
 except requests.exceptions.RequestException as e:
-    exit_with_msg(18, f"Access was not granted (Access denied). {e}")
+    exit_with_msg(11, f"Access was not granted (Access denied). {e}")
 
 http_code = response.status_code
 if http_code == 200:
@@ -152,17 +171,5 @@ if http_code == 200:
         else:
             print(key)
     sys.exit(0)
-elif http_code == 401:
-    exit_with_msg(41, "Login Failed (Access denied)")
-elif http_code == 402:
-    exit_with_msg(42, "Service ID not valid (Access denied)")
-elif http_code == 403:
-    exit_with_msg(43, "No assertion resulted from the AttributeQuery (Access denied)")
-elif http_code == 404:
-    exit_with_msg(44, "User is not registered (Access denied)")
-elif http_code == 500:
-    exit_with_msg(50, "Misconfigured service (Access denied)")
-else:
-    exit_with_msg(
-        19, f"Access was not granted (Access denied). HTTP status code is {http_code}."
-    )
+elif http_code != 200:
+    exit_with_msg(12, f"Access denied ({http_code})")
